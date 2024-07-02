b177070033b218da2ada633037e2dc47aa1dc6a6
Manav17s <143324011+Manav17s@users.noreply.github.com>
2024-07-02 23:46:40 +0530

    Delete pizza store/scripts/services/controllers/product-controller.js

--- pizza store/scripts/services/controllers/product-controller.js
+++
@@ -1,8 +0,0 @@
-// glue btw view(UI) and model
-import productOperations from '../services/product-operations.js';
-
-async function loadPizzas(){ 
-const pizzas =  await productOperations.loadProducts();
-console.log('Pizzas are',pizzas);
-}
-loadPizzas();
